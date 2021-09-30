190bac4f6788daf0bc037aba15c64c33740ce3b3
Yasin <yasinabdussaburr@gmail.com>
2021-09-30 09:04:57 -0400

    commit tree 049754f9a5343a8e6bc10de57e73f2ccd6d1f76f

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+  const [add, setAdd] = useState()
+  
+  const handleAddToCart = () => {
+  }
+
   return (
     <li className="">
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleAddToCart}>Add to Cart</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory] = useState('All')
+
+  const handleFilterChange = (e) => {
+    setSelectedCategory(e.target.value);
+
+  }
+  
+   const itemsToDisplay = items.filter((item) => {
+     if (selectedCategory === 'All') {
+    return true; }
+    
+        return items.category === selectedCategory
+    })
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleFilterChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
